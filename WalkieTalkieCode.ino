9738321d275636a45362c2cf4d41fac673ca300b
ElectronicsClubOSU <electronicsosu@gmail.com>
2018-11-27 19:19:58 -0500

    Update WalkieTalkieCode.ino

--- WalkieTalkieCode.ino
+++ WalkieTalkieCode.ino
@@ -1,63 +1,35 @@
 /*
-  FILENAME:   WalkieTalkieCode.ino
-  AUTHOR:     Chris Murray
-  CONTACT:    cdmurray08@gmail.com
-  
-  DESCRIPTION
-  Simple example for creating a walkie talkie by sending audio
-  using the nRF24L01 RF transceiver module.
-  This code enables multiple private channels to operate simultaneously in the same vicinity. 
-  
-  DISCLAIMER
-  This code is in the public domain. Please feel free to modify,
-  use, etc however you see fit. But, please give reference to
-  original authors as a courtesy to Open Source developers.
-  Modified example from RF24 Audio Library by TMRh20. Comments
-  from TMRh20 below. 
-  
-  Original code written by Orlando S. Hoilett, orlandohoilett@gmail.com
-  
-*/
+  FILENAME:   WalkieTalkieWorkshop.ino
+  DATE: November 28, 2018
+  Modified example from Orlando S. Hoilett, orlandohoilett@gmail.com 2015
+  RF24 Audio Library TMRh20 2014
 
-/* RF24 Audio Library TMRh20 2014
-This sketch is intended to demonstrate the basic functionality of the audio library.
-Requirements:
-2 Arduinos (Uno,Nano,Mega, etc supported)
-2 NRF24LO1 Radio Modules
-1 or more input devices (microphone, ipod, etc)
-1 or more output devices (speaker, amplifier, etc)
-Setup:
-1. Change the CE,CS pins below to match your chosen pins (I use 7,8 on 328 boards, and 48,49 on Mega boards)
-2. Upload this sketch to two or more devices
-3. Send serial commands via the serial monitor to control transmission and volume (volume only affects receiving devices)
-Default Pin Selections:
-Speaker: pins 9,10 on UNO, Nano,  pins 11,12 on Mega 2560
-Input/Microphone: Analog pin A0 on all boards
 */
 
 #include <userConfig.h>
 #include <RF24.h>
 #include <SPI.h>
 #include <RF24Audio.h>
-#include "printf.h"    // General includes for radio and audio lib
+#include "printf.h"    // General includes for radio audio lib
 
-//int radioID = ???;   //replace question marks with your radio identifier, 0-128
-
-RF24 radio(7,8);    // Set radio up using pins 7 (CE) 8 (CS)
-RF24Audio rfAudio(radio,0); // Set up the audio using the radio, and set to radio number 0
-
+int radioID = 50;     
 int talkButton = 3;
+
+RF24 radio(7,8);                    // Set radio up using pins 7 (CE) 8 (CS)
+RF24Audio rfAudio(radio,radioID);   // Set up the audio using the radio, and set radio identifier number to 'radioID'
+
 
 void setup() {      
   Serial.begin(115200);
+
+  
   
   printf_begin();
-  radio.begin();
-  radio.setChannel(radioID);
-  radio.printDetails();
-  rfAudio.begin();
-  rfAudio.broadcast(radioID);
-  
+  radio.begin();                    //initializes radio library
+  radio.setChannel(radioID);        //sets radio to address between 0 - 124, aka 2.4GHz- 2.525GHz
+  radio.printDetails();             //prints radio info to serial monitor
+  rfAudio.begin();                  //initializes radio audio library
+  rfAudio.broadcast(radioID);       //cestablish private radio channel
 
   pinMode(talkButton, INPUT);
 
@@ -72,7 +44,7 @@ void setup() {
 //void talk()
 //Called in response to interrupt. Checks the state of the button.
 //If the button is pressed (and held) enters transmit mode to send
-//audio. If button is release, enters receive mode to listen.
+//audio. If button is released, enters receive mode to listen.
 void talk()
 {
   if (digitalRead(talkButton)) rfAudio.transmit();
